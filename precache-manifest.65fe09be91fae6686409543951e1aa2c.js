ac193d7dfb94bcf8cde21ab95a9bb5208b8e4ccf
AJ Larson <ajlarson@AJsMac.local>
2020-10-10 11:00:47 -0600

    Updates

---
+++ precache-manifest.65fe09be91fae6686409543951e1aa2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5caab73b016d66907b0b1b4bab8e307c",
+    "url": "/qa_bookshelf/index.html"
+  },
+  {
+    "revision": "7ff1f9e763e9ec2bc611",
+    "url": "/qa_bookshelf/static/css/main.72b5ccd7.chunk.css"
+  },
+  {
+    "revision": "3ad125318c201d33857e",
+    "url": "/qa_bookshelf/static/js/2.41ead6dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qa_bookshelf/static/js/2.41ead6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff1f9e763e9ec2bc611",
+    "url": "/qa_bookshelf/static/js/main.265e1f88.chunk.js"
+  },
+  {
+    "revision": "87d11205e33c3396f10a",
+    "url": "/qa_bookshelf/static/js/runtime-main.9462eaaf.js"
+  }
+]);
